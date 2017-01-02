b3f01276a14f6fc181e537cf2396e21ce5793aa8
tonoeleinco <desarrollo3@eleinco.com.co>
2017-01-02 10:30:54 -0500

    Lee daatos de firebase cuando se presenta algun cambio

--- learningfirebase/src/main/java/com/example/learningfirebase/MainActivity.java
+++ learningfirebase/src/main/java/com/example/learningfirebase/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.learningfirebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +32,21 @@ public class MainActivity extends AppCompatActivity {
 
         Firebase.setAndroidContext(this);   //Se indica el contexto que puede utilizar firebase
         firebase = new Firebase(URL).child(CHILD);  //Se le da un valor a la variable firebase indicandole la url y el hijo o valor al que va acceder
+
+        //Para leer solo un valor cuando se presente un cambio
+        firebase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.getValue() != null){
+                    Toast.makeText(MainActivity.this, dataSnapshot.getValue().toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
 
         bEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
